f1d8561a4f1fac08eaebe07c824dfebca2bac66c
Sergey Maksimov <s.mksimov@gmail.com>
2026-01-13 07:31:11 +0700

    Change app url

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
 import { socialLinks } from "./socialLinks";
 
 export const siteConfig = {
-	name: "Sadhana Tracker",
-	description: "Осознанный трекер сна и привычек. Без браслетов и датчиков. Добавь в свою жизнь продуктивности, высвободи время для действительно важных дел.",
-	keywords: ["mobile app", "landing page"],
-	logo: "/assets/favicon.png",
-	storeLinks: {
-		apple: "https://sadhana-tracker.netlify.app",
-		google: "#",
-	},
-	socialLinks,
+  name: "Sadhana Tracker",
+  description: "Осознанный трекер сна и привычек. Без браслетов и датчиков. Добавь в свою жизнь продуктивности, высвободи время для действительно важных дел.",
+  keywords: ["mobile app", "landing page"],
+  logo: "/assets/favicon.png",
+  storeLinks: {
+    apple: "https://app.sadhana-tracker.com",
+    google: "#",
+  },
+  socialLinks,
 } as const;
